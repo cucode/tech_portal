8c35a78ee9a9e1a7d6292d4da47b701a78783e71
Andrew Janssen <m.andrew.janssen@gmail.com>
2013-10-31 12:04:30 -0500

    Bugfix: crash on GET site_root when there was no root Page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def show
-    page = Page.find_by_url(params[:url] || '/')
-    render text: page.content.html_safe, layout: true
+    text = (Page.find_by_url(params[:url] || "/").try(:content).try(:html_safe)) || "Page not found"
+    render text: text, layout: true
   end
 end
