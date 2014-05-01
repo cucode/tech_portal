6ee378678e422a6d402f012ce21c87ea12e32857
Andrew Janssen <m.andrew.janssen@gmail.com>
2014-04-30 22:04:35 -0500

    Code formatting.

--- app/models/event.rb
+++ app/models/event.rb
@@ -16,13 +16,13 @@ class Event < ActiveRecord::Base
   # Validations
 
   validates :blog_content, presence: true, if: :event_photo_url
-  validates :summary,     presence: true
-  validates :description, presence: true
-  validates :dtend,       presence: true
-  validates :dtstart,     presence: true
-  validates :feed,        presence: true
-  validates :uid,         presence: true, uniqueness: true
-  validates :url,         presence: true
+  validates :summary,      presence: true
+  validates :description,  presence: true
+  validates :dtend,        presence: true
+  validates :dtstart,      presence: true
+  validates :feed,         presence: true
+  validates :uid,          presence: true, uniqueness: true
+  validates :url,          presence: true
 
 
 end
